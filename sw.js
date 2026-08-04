0ee2bc15e645095c8cf767a43b759ae383c04018
Ilia <fylymonov@brazy.gg>
2026-08-04 13:28:25 +0200

    Mobilcard PWA prototype: QR screen + cable car animation

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'mobilcard-v1';
+const ASSETS = ['./', './index.html', './qr.svg', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
