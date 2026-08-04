5c61c0d2c341047755b7fe5df4294865770a7bbf
Ilia <fylymonov@brazy.gg>
2026-08-04 23:11:15 +0200

    Exact scene+cabin from reference SVGs, per-passenger QR (Illia real ticket), timer from 00:00

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'mobilcard-v10';
-const ASSETS = ['./', './index.html', './qr.svg', './manifest.webmanifest', './icon-180.png', './icon-512.png', './logo.png'];
+const CACHE = 'mobilcard-v12';
+const ASSETS = ['./', './index.html', './qr.svg', './manifest.webmanifest', './icon-180.png', './icon-512.png', './logo.png', './qr-ilia.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
