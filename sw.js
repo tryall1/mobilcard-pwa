887d24a5fce943bb46febf89b0115a322146f17b
Ilia <fylymonov@brazy.gg>
2026-08-04 19:32:20 +0200

    Tickets list screen with working close navigation, real logo, icons from logo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'mobilcard-v2';
-const ASSETS = ['./', './index.html', './qr.svg', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
+const CACHE = 'mobilcard-v3';
+const ASSETS = ['./', './index.html', './qr.svg', './manifest.webmanifest', './icon-180.png', './icon-512.png', './logo.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
