d6d4a2384992095ff6222e7cd8b02b50b5383fe7
Ilia <fylymonov@brazy.gg>
2026-08-04 19:50:59 +0200

    Ticket icon instead of pencil (Show details, header, Subscription tab)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mobilcard-v6';
+const CACHE = 'mobilcard-v7';
 const ASSETS = ['./', './index.html', './qr.svg', './manifest.webmanifest', './icon-180.png', './icon-512.png', './logo.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
