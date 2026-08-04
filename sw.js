878b4db936aa97fca34cd37139033ff1999d1e8b
Ilia <fylymonov@brazy.gg>
2026-08-04 18:33:08 +0200

    Real QR from original ticket, validation date +1 day, cache bump

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mobilcard-v1';
+const CACHE = 'mobilcard-v2';
 const ASSETS = ['./', './index.html', './qr.svg', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
